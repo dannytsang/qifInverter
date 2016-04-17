3a598d2e4473a785dc972365c20509f1833faa3b
Danny Tsang <danny@dannytsang.co.uk>
2016-04-17 21:22:17 +0100

    Removed unused imports from ConfigurationManagerTest.

--- src/test/java/uk/co/dannytsang/qifinverter/configuration/ConfigurationManagerTest.java
+++ src/test/java/uk/co/dannytsang/qifinverter/configuration/ConfigurationManagerTest.java
@@ -1,8 +1,6 @@
 package uk.co.dannytsang.qifinverter.configuration;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class ConfigurationManagerTest {
