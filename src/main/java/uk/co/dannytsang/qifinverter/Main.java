30b11c190d646797186250c84538711b04f772de
Danny Tsang <danny@dannytsang.co.uk>
2016-04-17 21:42:13 +0100

    Fix build with Main class file.

---
+++ src/main/java/uk/co/dannytsang/qifinverter/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package uk.co.dannytsang.qifinverter;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Main method starting class for application.
+ * 
+ * @author dannytsang<danny@dannytsang.co.uk>
+ *
+ */
+public class Main {
+
+	private static final Logger LOG = LogManager.getLogger(Main.class);
+
+	/**
+	 * @param args
+	 *          Command line parameters. [1] file path to qif (required). [2]
+	 *          output file (optional).
+	 */
+	public static void main(String[] args) {
+		/* TODO */
+
+	}
+
+}
